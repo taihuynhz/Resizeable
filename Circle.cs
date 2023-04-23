0951702db0dd53c7f5d41b2d7181d36f3e0f8023
taihuynhz <129383417+taihuynhz@users.noreply.github.com>
2023-04-23 14:59:23 +0700

    Resizeable

---
+++ Circle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resizeable
+{
+    public class Circle : Shape, Resizeable
+    {
+        private double radius = 1.0;
+
+        public Circle()
+        {
+
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public Circle(double radius, String color, bool filled) : base(color, filled)
+        {
+            this.radius = radius;
+        }
+
+        public void Resize()
+        {
+            Random random = new Random();
+            this.radius = this.radius + this.radius * random.Next(1, 100) / 100;
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+
+        public void setRadius(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+
+        public double getPerimeter()
+        {
+            return 2 * radius * Math.PI;
+        }
+
+        public override string ToString()
+        {
+            return "A Circle with radius="
+                    + getRadius()
+                    + ", which is a subclass of "
+                    + base.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resizeable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Circle Test
+            Circle[] circles = new Circle[3];
+
+            circles[0] = new Circle();
+            circles[1] = new Circle(3.5);
+            circles[2] = new Circle(7, "indigo", false);
+
+            Console.WriteLine("====================================================================================================");
+            Console.WriteLine("Circle Test");
+            Console.WriteLine("====================================================================================================");
+           
+            Console.WriteLine("Before resize:");
+            foreach (Circle circle in circles)
+            {
+                Console.WriteLine(circle);
+            }
+
+            Console.WriteLine("After resize:");
+            foreach (Circle circle in circles)
+            {
+                circle.Resize();
+                Console.WriteLine(circle);
+            }
+
+            // Rectangle Test
+            Rectangle[] rectangles = new Rectangle[3];
+
+            rectangles[0] = new Rectangle();
+            rectangles[1] = new Rectangle(3.5, 7);
+            rectangles[2] = new Rectangle(7, 10.5, "red", false);
+
+            Console.WriteLine("====================================================================================================");
+            Console.WriteLine("Rectangle Test");
+            Console.WriteLine("====================================================================================================");
+
+            Console.WriteLine("Before resize:");
+            foreach (Rectangle rectangle in rectangles)
+            {
+                Console.WriteLine(rectangle);
+            }
+
+            Console.WriteLine("After resize:");
+            foreach (Rectangle rectangle in rectangles)
+            {
+                rectangle.Resize();
+                Console.WriteLine(rectangle);
+            }
+
+            // Square Test
+            Square[] squares = new Square[3];
+
+            squares[0] = new Square();
+            squares[1] = new Square(2.3);
+            squares[2] = new Square(5.8, "yellow", true);
+
+            Console.WriteLine("====================================================================================================");
+            Console.WriteLine("Square Test");
+            Console.WriteLine("====================================================================================================");
+
+            Console.WriteLine("Before resize:");
+            foreach (Square square in squares)
+            {
+                Console.WriteLine(square);
+            }
+
+            Console.WriteLine("After resize:");
+            foreach (Square square in squares)
+            {
+                square.Resize();
+                Console.WriteLine(square);
+            }
+        }        
+    }
+}
